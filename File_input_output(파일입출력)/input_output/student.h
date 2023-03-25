990f91fa965e6b835e50fb2af2b6f2b1781dfa7a
Hyun <hickee32@gmail.com>
2023-03-25 21:18:03 +0900

    input_output

---
+++ File_input_output(파일입출력)/input_output/student.h
@@ -0,0 +1,24 @@
+//#pragma once
+#ifndef  __STUDENT_H__
+#define  __STUDENT_H__
+
+typedef struct _STUDENT {
+	//_STUDENT ST 같으면 안된다.
+
+	char name[10]; //이름
+	int kor;
+	int eng;
+	int math;
+	int hap;
+	double avg;
+	char grade;
+
+} ST;
+
+ST st; //구조체 변수 선언 (전역 변수)
+
+extern void init_student(); //함수 프로토 타입
+void show_student(ST st); //extern 생략 가능
+
+#endif
+
