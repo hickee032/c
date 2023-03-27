209e1b0d5e24b48061ebf1c79dbb5fffef9e2553
Hyun <hickee32@gmail.com>
2023-03-27 15:55:30 +0900

    modular

---
+++ modular(모듈화)/modul/def.h
@@ -0,0 +1,10 @@
+//#pragma once  //MS soft의 방식
+//전처리기 컴파일러 처리 전에 처리해달라?
+//sork aksemf gpej
+
+#ifndef __DEF_H__ // 정의되지 않으면 if not define
+#define __DEF_H__
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#endif
